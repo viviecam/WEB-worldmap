713361c29a335cc82e3203e8526e29905c4323c4
Camille Vivier <viviecam@iut1i-100-a15.local>
2017-11-22 15:28:38 +0100

    Add modal - flags

--- js/index.js
+++ js/index.js
@@ -4,12 +4,17 @@ $(document).ready(function(){
 		// var url = "https://restcountries.eu/rest/v2/all";
 		var url = "https://restcountries.eu/rest/v2/alpha?codes=" + alpha;
 		$.getJSON(url, function(data){
-			console.log(data[0].translations.fr);
+			// console.log(data[0].translations.fr);
+			$('.modal-title').html(data[0].translations.fr);
+			console.log(data[0].flag);
+			var urlImg = data[0].flag;
+			$('.modal-body img').attr("src", urlImg);
+			// $('.modal-body img').attr('src', data[0].flag);
 		});
 	}
 
 	$('path').click(function(){
-		var alpha = $(this).attr( "class" );
+		var alpha = $(this).attr( "id" );
 		request(alpha);
 	});
 });
